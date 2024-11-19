361a9f1cdaf374a4a691725d4f2fdfa6c106620d
Pedro Ferreira <devbueno3@gmail.com>
2024-11-19 19:17:54 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/")
+  }
+
   return ( 
+
+
     <div className="h-full grid grid-cols-2">
       {/* ESQUERDA */}
       <div className="flex flex-col h-full justify-center p-8 max-w-[550px] mx-auto">
@@ -11,11 +22,13 @@ const LoginPage = () => {
         <h1 className="text-4xl font-bold mb-3">Bem vindo!</h1>
         <p className="text-muted-foreground mb-8">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.
-        </p> 
-        <Button variant="outline">
-          <LogInIcon className="mr-2"/>
-          Fazer login ou criar conta
-        </Button>
+        </p>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2"/>
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
 
       {/* DIREITA */}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const HomePage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login")
+  }
 
 
-export default function Home() {
   return (
-    <Button>Hello word</Button>
+    <div className="h-full flex items-center justify-center">
+      <UserButton showName/>
+    </div>
   );
 }
+ 
+export default HomePage;
