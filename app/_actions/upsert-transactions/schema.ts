bc2070f7a42f43e6d7443fe95835c12b2eef7e1e
Pedro Ferreira <devbueno3@gmail.com>
2024-12-04 10:23:32 -0300

    refactor: add message erro in schema addTransactionSchema

--- app/_actions/upsert-transactions/schema.ts
+++ app/_actions/upsert-transactions/schema.ts
@@ -2,10 +2,10 @@ import { TransactionCategory, TransactionPaymentMethod, TransactionType } from "
 import { z } from "zod";
 
 export const addTransactionSchemas = z.object({
-  name: z.string().trim().min(1),
-  amount: z.number().positive(),
-  type: z.nativeEnum(TransactionType),
-  category: z.nativeEnum(TransactionCategory),
-  paymentMethod: z.nativeEnum(TransactionPaymentMethod),
+  name: z.string().trim().min(1, { message: "Transaction name is required." }),
+  amount: z.number().positive({ message: "Amount must be a positive number." }),
+  type: z.nativeEnum(TransactionType, { message: "Invalid transaction type." }),
+  category: z.nativeEnum(TransactionCategory, { message: "Invalid transaction category." }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, { message: "Invalid payment method." }),
   date: z.date()
 })
