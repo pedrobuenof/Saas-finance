0829b8d46189751bea864a3734d85e75de8a843e
Pedro Ferreira <devbueno3@gmail.com>
2024-12-04 15:44:04 -0300

    refactor: centralize authentication logic and optimize queries

--- app/_data/can-user-add-transaction/index.ts
+++ app/_data/can-user-add-transaction/index.ts
@@ -1,17 +1,23 @@
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { getCurrentMonthTransactions } from "../get-current-month-transactions";
 
-export const canUserAddTransaction = async () => {
-  const [{ userId }, currentMonthTransactions] = await Promise.all([
-    auth(),
-    getCurrentMonthTransactions(),
-  ]);
+const getUserId = () => {
+  const { userId } = auth();
 
   if (!userId) {
-    throw new Error("Unauthorized");
+    throw new Error("User is not ");
   }
 
-  const user = await clerkClient().users.getUser(userId);
+  return userId
+}
+
+export const canUserAddTransaction = async () => {
+  const userId = getUserId();
+
+  const [currentMonthTransactions, user] = await Promise.all([
+    getCurrentMonthTransactions(),
+    clerkClient().users.getUser(userId),
+  ]);
 
   if (user.publicMetadata.subscriptionPlan === "premium") {
     return true;
