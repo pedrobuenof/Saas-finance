9411f527c888e2ef022133911a64032961b3c839
Pedro Ferreira <devbueno3@gmail.com>
2024-12-04 15:44:36 -0300

    refactor: centralize authentication logic in get-current-month-transaction

--- app/_data/get-current-month-transactions/index.ts
+++ app/_data/get-current-month-transactions/index.ts
@@ -2,11 +2,19 @@ import { db } from "@/app/_lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { endOfMonth, startOfMonth } from "date-fns";
 
-export const getCurrentMonthTransactions = async () => {
-  const { userId } = await auth();
+const getUserId = () => {
+  const { userId } = auth();
+
   if (!userId) {
-    throw new Error("Unauthorized");
+    throw new Error("User is not ");
   }
+
+  return userId
+}
+
+export const getCurrentMonthTransactions = async () => {
+  const userId = getUserId();
+
   return db.transaction.count({
     where: {
       userId,
